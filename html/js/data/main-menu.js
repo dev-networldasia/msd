6e7e6d91e5547a1f259c6ae222c05fac4482ef63
Henry Nguyen <155721321+luanminhnguyen296@users.noreply.github.com>
2024-08-01 15:36:27 +0700

    [MSD-001] dev-fe done clone feature search

---
+++ html/js/data/main-menu.js
@@ -0,0 +1,81 @@
+var mainMenuData = [
+    {
+        id: 1,
+        title: "Nữ giới",
+        link: "#",
+    },
+    {
+        id: 2,
+        title: "Nam giới",
+        link: "#",
+    },
+    {
+        id: 3,
+        title: "Trắc nghiệm",
+        link: "#",
+    },
+    {
+        id: 4,
+        title: "Sống lành chủ động",
+        link: "#",
+    },
+    {
+        id: 5,
+        title: "Hoạt động cộng đồng",
+        link: "#",
+    },
+    {
+        id: 6,
+        title: "Địa điểm tư vấn",
+        link: "#",
+    },
+];
+
+var searchResults = {
+    status: 1,
+    total: 22,
+    message: "Danh sách bài viết!",
+    data: [
+        {
+            id: 54,
+            title: "HPV VÀ NHỮNG ĐIỀU CẦN BIẾT Ở CỘNG ĐỒNG LGBTQ+",
+            url: "hpv-va-nhung-dieu-can-biet-o-cong-dong-lgbtq",
+            categoryId: 6,
+            categoryUrl: "du-phong-hpv-cho-nam-tu-27-45-tuoi",
+            maxColumn: 621,
+        },
+        {
+            id: 43,
+            title: "BỆNH KHÓ LƯỜNG CỦA PHÁI NAM: UNG THƯ DƯƠNG VẬT",
+            url: "benh-kho-luong-cua-phai-nam-ung-thu-duong-vat",
+            categoryId: 6,
+            categoryUrl: "du-phong-hpv-cho-nam-tu-27-45-tuoi",
+            maxColumn: 621,
+        },
+        {
+            id: 45,
+            title: "NHỮNG SỰ THẬT GIẬT MÌNH VỀ HPV Ở NAM GIỚI, BẠN ĐÃ BIẾT?",
+            url: "nhung-su-that-giat-minh-ve-hpv-o-nam-gioi-ban-da-biet",
+            categoryId: 5,
+            categoryUrl: "du-phong-hpv-cho-nam-tu-18-26-tuoi",
+            maxColumn: 0,
+        },
+        {
+            id: 61,
+            title: "VÌ SAO UNG THƯ CỔ TỬ CUNG LÀ VẤN ĐỀ SỨC KHỎE CẦN ĐƯỢC QUAN TÂM (1) TỶ LỆ PHỤ NỮ MẮC UNG THƯ CỔ TỬ CUNG VẪN CÒN CAO TẠI VIỆT NAM (2)",
+            url: "vi-sao-ung-thu-co-tu-cung-la-van-de-suc-khoe-can-duoc-quan-tam-1-ty-le-phu-nu-mac-ung-thu-co-tu-cung-van-con-cao-tai-viet-nam-2",
+            categoryId: 3,
+            categoryUrl: "du-phong-hpv-cho-nu-tu-27-45-tuoi",
+            maxColumn: 0,
+        },
+        {
+            id: 60,
+            title: "ĐỪNG MẮC UNG THƯ CỔ TỬ CUNG CHỈ VÌ QUAN NIỆM SAI LẦM VỀ PHÒNG NGỪA HPV",
+            url: "dung-mac-ung-thu-co-tu-cung-chi-vi-quan-niem-sai-lam-ve-phong-ngua-hpv",
+            categoryId: 3,
+            categoryUrl: "du-phong-hpv-cho-nu-tu-27-45-tuoi",
+            maxColumn: 0,
+        },
+    ],
+};
+
--- html/js/layout/main-menu.js
+++ html/js/layout/main-menu.js
@@ -1,42 +1,9 @@
-const cards = [
-    {
-        id: 1,
-        title: "Nữ giới",
-        link: "#",
-    },
-    {
-        id: 2,
-        title: "Nam giới",
-        link: "#",
-    },
-    {
-        id: 3,
-        title: "Trắc nghiệm",
-        link: "#",
-    },
-    {
-        id: 4,
-        title: "Sống lành chủ động",
-        link: "#",
-    },
-    {
-        id: 5,
-        title: "Hoạt động cộng đồng",
-        link: "#",
-    },
-    {
-        id: 6,
-        title: "Địa điểm tư vấn",
-        link: "#",
-    },
-];
-
 function renderListMenuDesktop() {
     const menuEl = document.querySelector(".menu-desktop #nav-menu ul");
 
     if (!menuEl) return console.log("Not found element!");
 
-    cards.forEach((item) => {
+    mainMenuData.forEach((item) => {
         const li = document.createElement("li");
         li.className = "nav-menu__item";
 
@@ -47,3 +14,76 @@ function renderListMenuDesktop() {
     });
 }
 renderListMenuDesktop();
+
+function addEventForButtonSearch() {
+    const elBtn = document.querySelector(".nav-menu #search-control");
+
+    if (!elBtn) return console.log("Not found element!");
+
+    elBtn.addEventListener("click", (e) => {
+        createPopupSearch();
+    });
+
+    document.addEventListener("click", (event) => {
+        if (elBtn.contains(event.target)) return;
+
+        const containerSearch = document.querySelector(
+            "#popup-search #container-search"
+        );
+        if (!containerSearch.contains(event.target)) {
+            removePopupSearch();
+        }
+    });
+}
+
+function createPopupSearch() {
+    const popupSearchEle = document.createElement("div");
+    popupSearchEle.className = "popup-search";
+    popupSearchEle.id = "popup-search";
+    popupSearchEle.innerHTML = `
+    <div class="overlay"></div>
+        <div class="container-search box-shadow-md" id="container-search">
+            <input type="text" placeholder="Tìm kiếm..." />
+            <div class="search-results" id="search-results">
+
+            </div>
+            <div class="no-results">
+                Không tìm thấy kết quả nào
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(popupSearchEle);
+
+    renderListSearchResult();
+}
+
+function removePopupSearch() {
+    const popupSearchEle = document.querySelector("#popup-search");
+    if (popupSearchEle) document.body.removeChild(popupSearchEle);
+}
+
+function renderListSearchResult() {
+    const searchResultEl = document.querySelector("#search-results");
+
+    if (!searchResultEl) return console.log("Not found element!");
+
+    const { status, total, message, data } = searchResults;
+
+    if (status === 0) {
+        searchResultEl.innerHTML = `<p class="text-danger">${message}</p>`;
+        return;
+    }
+
+    const list = data.map((item) => {
+        const link = `<a href="${item.url}">${item.title}</a>`;
+        return `<li class="search-result__item">${link}</li>`;
+    });
+
+    searchResultEl.innerHTML = `<ul>${list.join("")}</ul>`;
+}
+
+setTimeout(() => {
+    renderListSearchResult();
+    addEventForButtonSearch();
+}, 1000);
