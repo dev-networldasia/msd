0e30597337ae63f2722df39ba9101fdb667a5c6d
Khang Huynh <khang.vtvst@gmail.com>
2024-08-05 13:49:47 +0700

    male-9-18

---
+++ html/js/pages/male-9-18/sidebar.js
@@ -0,0 +1,39 @@
+function renderListCard() {
+    const listCardEl = document.querySelector("#sidebar-list-card");
+
+    if (!listCardEl) return console.log("Not found element!");
+
+    homeSidebarCards.forEach((card) => {
+        const item = document.createElement("div");
+        item.className = "card-item box-shadow-sm";
+        const contentCardItem = `
+               <div class="avatar">
+                  <img src="${card.image}" />
+               </div>
+               <div class="info">
+                  <h3 class="title">
+                     ${card.title}
+                  </h3>
+                  <p class="subtitle">
+                     ${card.subtitle}
+                  </p>
+                  <p class="description">${card.description}</p>
+                  <div class="actions">
+                     <a href="${card.link_female}" class="btn btn-primary-outline full-radius">
+                        <p>Tìm hiểu thêm</p>
+                        <b>Nữ</b>
+                     </a>
+
+                     <a href="${card.link_male}" class="btn btn-primary-outline full-radius">
+                        <p>Tìm hiểu thêm</p>
+                        <b>Nam</b>
+                     </a>
+                  </div>
+               </div>
+            `;
+        item.innerHTML = contentCardItem;
+
+        listCardEl.appendChild(item);
+    });
+}
+renderListCard();
